7a043d02d6a145751ea48892a5d5c91376f60073
rchertara <rchertara@gmail.com>
2018-11-21 22:28:26 -0500

    mod page done front end and middle side

--- bar-beer-drinker-ui/src/app/modification-page/modification-page.component.ts
+++ bar-beer-drinker-ui/src/app/modification-page/modification-page.component.ts
@@ -126,6 +126,7 @@ export class ModificationPageComponent implements OnInit {
         var response:string=data.toString()
         if(response==='Change has been successful'){
           this.show();
+          this.loadAllTables();
         }
         else{
           this.msgs=[];
@@ -158,6 +159,77 @@ export class ModificationPageComponent implements OnInit {
     this.msgs.push(m);
   }
 
+  loadAllTables(){
+    this.barService.getBars().subscribe(
+      data => {
+        this.allBars=data;
+      },
+      error => {
+        alert('Could not retrieve a list of bars');
+      }
+    );
+    this.transService.getTransactions().subscribe(
+      data => {
+        this.allDrinkers = data;
+
+      }
+    );
+    this.transService.getItems().subscribe(
+      data => {
+        this.allItems = data;
+
+      }
+    );
+    this.barService.getBartenders().subscribe(
+      data => {
+        this.allBartenders = data;
+
+      }
+    );
+    this.transService.getBills().subscribe(
+      data => {
+        this.allBills= data;
+
+      }
+    );
+    this.transService.getFreq().subscribe(
+      data => {
+        this.allFreq= data;
+
+      }
+    );
+    this.transService.getLikes().subscribe(
+      data => {
+        this.allLikes= data;
+
+      }
+    );
+    this.transService.getops().subscribe(
+      data => {
+        this.allops= data;
+
+      }
+    );
+    this.transService.getworks().subscribe(
+      data => {
+        this.allworks= data;
+
+      }
+    );
+    this.transService.getsells().subscribe(
+      data => {
+        this.allsells= data;
+
+      }
+    );
+    this.transService.gettrans().subscribe(
+      data => {
+        this.allTrans= data;
+
+      }
+    );
+
+  }
 
 
 }
